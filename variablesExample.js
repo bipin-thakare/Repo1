cab51931bc44fbed955ba546a06ddb4f15c2fe4d
bipin-thakare <bipin.thakare@gmail.com>
2020-09-28 13:52:03 +0530

    my first commit

---
+++ variablesExample.js
@@ -0,0 +1,14 @@
+// let i=10;
+// console.log(i);
+
+// let str1="javascript";
+//  let x=32, y="hello there";
+
+// console.log(x + y);
+
+let arr=['aaa','bbb','ccc'];
+
+for(let i=0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
